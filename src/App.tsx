2e4f326d3a49e93caaa115d6587626d376e5faa1
Irina <irinastefan@Stefans-iMac.local>
2024-09-30 18:24:04 +0300

    Platform Selector

--- src/App.tsx
+++ src/App.tsx
@@ -4,6 +4,7 @@ import GameGrid from "./components/GameGrid";
 import GenreList from "./components/GenreList";
 import { useState } from "react";
 import { Genre } from "./hooks/useGenres";
+import PlatformSelector from "./components/PlatformSelector";
 
 //17-Replace Aside with GenreList component
 //19-Add padding to the <GridItem>
@@ -37,6 +38,7 @@ function App() {
       </Show>
 
       <GridItem area="main">
+        <PlatformSelector />
         <GameGrid selectedGenre={selectedGenre} />
       </GridItem>
     </Grid>
